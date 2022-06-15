ca738f095335319ff9db30733b78897986cb7731
saurabh0145 <69715411+saurabh0145@users.noreply.github.com>
2022-06-15 15:32:34 +0530

    Add files via upload

---
+++ jenkins-demo/src/main/java/com/training/jenkinsdemo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.training.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+	
+	@GetMapping("/msg")
+	public String getMsg() {
+		return "Hello from spring boot!";
+	}
+}
